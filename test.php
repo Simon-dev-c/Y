476aefa93c6f10787591fdc2e359324fc548e9d2
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-03-24 17:13:26 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>Y</title>
+    </head>
+    <body>
+        <h1>
+            Y
+        </h1>
+    </body>
+</html>
