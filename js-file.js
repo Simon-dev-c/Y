f6e18ee058252b13513239ecea3cb15fb3e45801
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-03-28 17:59:27 +0100

    Add files via upload

---
+++ js-file.js
@@ -0,0 +1,7 @@
+function aujourdhui(){
+    var maintenant = new Date();
+    var jour=maintenant.getDate();
+    var mois=maintenant.getMonth()+1;
+    var an=maintenant.getFullYear();
+    document.write(jour,"/",mois,"/",an);
+}
