41bf07d1a239750594c217f373ba64fb59e46575
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-03-28 17:33:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <link href="accueil.css" rel="stylesheet" type="text/css">
+        <title>Y</title>
+    </head>
+    <body>
+        <div class="banniere">
+            <h1>
+                Y
+            </h1>
+            <br>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="news.php">News</a>
+                    </li>
+                    <li>
+                        <a href="forum.php">Forum</a>
+                    </li>
+                    <li>
+                        <a href="suivis.php">Suivis</a>
+                    </li>
+                    <li>
+                        <a href="compte.php">Compte</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+        <br><br><br><br><br>
+    </body>
+</html>
