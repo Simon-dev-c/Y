bf22447ba5dc9468c5b7347db130b4e4f13596a8
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-03-28 17:59:41 +0100

    Delete js-file.js

--- js-file.js
+++
@@ -1,7 +0,0 @@
-function aujourdhui(){
-    var maintenant = new Date();
-    var jour=maintenant.getDate();
-    var mois=maintenant.getMonth()+1;
-    var an=maintenant.getFullYear();
-    document.write(jour,"/",mois,"/",an);
-}
