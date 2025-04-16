b306bc5650d471036f783d629d44c79088da2b12
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-16 17:18:03 +0200

    Option connecté/pas connecté

--- register.php
+++ register.php
@@ -17,21 +17,31 @@ $statut = $_SESSION['statut'] ?? null;
             <div class="logo1">
                 <img src="images/logo.jpg" alt="logo" width="70" height="70">
             </div>
-            <h2>S'identifier</h2>
-            
-            <label class="civilite">Civilité :<input type="radio" name="civilite" value="mme">Madame</label>
-            <label><input type="radio" name="civilite" value="m">Monsieur</label>
-            <br>
-            <label><input type="text" name="identifiant" placeholder="identifiant" required="required"></label><br>
-            <br>
-            <label><input type="email" name="email" placeholder="Adresse email" required="required"></label><br><br>
-            <label class="date">Date de naissance</label>
-            <select id="jour" name="jour"></select>
-            <select id="mois" name="mois"></select>
-            <select id="annee" name="annee"></select><br>
-            <label><input type="password" name="mdp" placeholder="Mot de passe" required="required" maxlength="25"></label><br><br>          
-            <label><input name="button" type="button" value="S'identifier"></label><br>
-            <label><a href="login.php">Se connecter</a></label><br>
+            <?php 
+            if (!isset($pseudo)){
+            ?>
+                <h2>S'identifier</h2>
+                <label class="civilite">Civilité :<input type="radio" name="civilite" value="mme">Madame</label>
+                <label><input type="radio" name="civilite" value="m">Monsieur</label>
+                <br>
+                <label><input type="text" name="identifiant" placeholder="identifiant" required="required"></label><br>
+                <br>
+                <label><input type="email" name="email" placeholder="Adresse email" required="required"></label><br><br>
+                <label class="date">Date de naissance</label>
+                <select id="jour" name="jour"></select>
+                <select id="mois" name="mois"></select>
+                <select id="annee" name="annee"></select><br>
+                <label><input type="password" name="mdp" placeholder="Mot de passe" required="required" maxlength="25"></label><br><br>          
+                <label><input name="button" type="button" value="S'identifier"></label><br>
+                <label><a href="login.php">Se connecter</a></label><br>
+            <?php
+            }else{
+                ?>
+                <h2>Vous êtes déjà connecté !</h2>
+                <a href='accueil.php'>Accueil</a><br>
+                <?php
+            }
+            ?>
         </form>
         <script src="js.js"></script>
     </body>
