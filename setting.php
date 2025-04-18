634dbedf7c7804cc5ab217809380a6d3d240708c
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-18 13:40:33 +0200

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,60 @@
+<?php
+session_start();
+$pseudo = $_SESSION["pseudo"] ?? null;
+$mdp = $_SESSION["mdp"] ?? null;
+$statut = $_SESSION['statut'] ?? null;
+
+function NoConnect(){
+    echo "Tu n'es pas connecté<br>";
+    echo "<a href='register.php'>Connexion</a><br>";
+    echo "<a href='login.php'>Inscription</a><br>";
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Project Y</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="header">
+            <nav class="profil">
+                <a href="profile.php"><img src="images/person-circle-outline.svg" alt="profil" width="70" height="70"></a>
+            </nav>
+            <div class="logo">
+                <img src="images/logo.jpg" alt="logo" width="70" height="70">
+            </div>
+            <?php
+            if (isset($pseudo)){
+            ?>
+                <div class="log-out">
+                    <a href="login.php"><img src="images/log-out.svg" alt="log-out" width="70" height="70"></a>
+                </div>
+            <?php
+            }
+            ?>
+            
+        </div>
+
+        <br><br><br><br><br> <!-- Important pour ne pas mettre le contenu derrière le header -->
+        <?php
+        if (!isset($pseudo)){
+            NoConnect();
+        }
+        ?>
+
+        <div class="footer">
+            <nav class="navigateur">
+                <ol>
+                    <li class="lien"><a href="accueil.php"><img src="images/home.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="setting.php"><img src="images/settings.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="message.php"><img src="images/paper-plane.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="partage.php"><img src="images/share.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                </ol>
+            </nav>
+        </div>
+    </body>
+</html>
