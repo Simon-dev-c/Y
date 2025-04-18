0c0783e2a2eb700ccaa46f164a23df0e92de8c9d
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-18 13:38:34 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -90,9 +90,9 @@ function NoConnect(){
             <nav class="navigateur">
                 <ol>
                     <li class="lien"><a href="accueil.php"><img src="images/home.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="#"><img src="images/settings.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="#"><img src="images/paper-plane.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="#"><img src="images/share.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="setting.php"><img src="images/settings.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="message.php"><img src="images/paper-plane.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="partage.php"><img src="images/share.svg" alt="profil" class="image" width="42" height="42"></a></li>
                 </ol>
             </nav>
         </div>
