d6ec728f1dc057a91c42648f4e53bcfb225cc9aa
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-18 13:39:32 +0200

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -51,10 +51,10 @@ function NoConnect(){
         <div class="footer">
             <nav class="navigateur">
                 <ol>
-                    <li class="lien"><a href="#"><img src="images/home.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="#"><img src="images/settings.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="#"><img src="images/paper-plane.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="#"><img src="images/share.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="accueil.php"><img src="images/home.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="setting.php"><img src="images/settings.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="message.php"><img src="images/paper-plane.svg" alt="profil" class="image" width="42" height="42"></a></li>
+                    <li class="lien"><a href="partage.php"><img src="images/share.svg" alt="profil" class="image" width="42" height="42"></a></li>
                 </ol>
             </nav>
         </div>
