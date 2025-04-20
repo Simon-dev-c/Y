07a1392471ea4965f2788365e0554d3c07148b7d
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-20 14:01:41 +0200

    Add files via upload

---
+++ sql/connexion.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+if (isset($_POST['nom']) && isset($_POST['mdp']) ) {
+    $nom = $_POST['nom'];
+    $mdp = $_POST['mdp'];
+
+    include('../includes/connexion_inc.php');
+    $pdo = connexion('../Y_database.db');
+    try {
+        $stmt = $pdo->prepare('SELECT nom,statut FROM users
+        WHERE nom == :nom
+        AND mot_de_passe == :mot_de_passe');
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':mot_de_passe', $mdp);
+
+        $stmt->execute();
+        $requete = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        echo "<pre>";
+        var_dump($requete);
+        echo "</pre>";
+
+        if (isset($requete['nom']) && isset($requete['statut'])) {
+            echo '<p>Connexion effectué</p>';
+            
+            $_SESSION['nom'] = $nom;
+            $_SESSION['statut'] = $requete["statut"];
+        } else {
+            echo '<p>Erreur</p>';
+        }
+    } catch (PDOException $e) {
+        echo '<p>Problème PDO</p>';
+        echo $e->getMessage();
+    }
+    $stmt->closeCursor();
+    $pdo = null;
+    ?>
+    <a href='../accueil.php'>Accueil</a><br>
+    <?php
+} else {
+    echo "<p>Mauvais paramètres</p>";
+}
+?>
