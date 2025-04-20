105f51c26218cd206ab171b6002a3b33e55cb3c4
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-20 12:01:53 +0200

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+$nom = $_SESSION["nom"] ?? null;
+$mdp = $_SESSION["mdp"] ?? null;
+$statut = $_SESSION['statut'] ?? null;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+    session_unset();
+    session_destroy();
+    header('Location: accueil.php');
+    $nom = null;
+    $mdp = null;
+    $statut = null;
+    exit();
+}
+
+
+include("includes/head2.php");
+?>
+
+<body>
+    <form method="POST">
+        <div class="logo1">
+            <img src="images/logo.jpg" alt="logo" width="70" height="70">
+        </div>
+        <?php
+        if (!isset($nom)) {
+        ?>
+            <h2>Vous n'êtes pas connecté !</h2>
+        <?php
+        } else {
+        ?>
+            <h2>Se déconnecter</h2>
+            <button type="submit" name="logout">Se déconnecter</button>
+            <br><br>
+            <label><a href="accueil.php">Accueil</a></label>
+        <?php
+        }
+        ?>
+    </form>
+</body>
+
+</html>
