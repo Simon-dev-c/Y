6c5a3ebe25ed1eff5285248c67976dcc7f06e0c7
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-20 11:48:58 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,100 +1,89 @@
 <?php
 session_start();
-$pseudo = $_SESSION["pseudo"] ?? null;
+$nom = $_SESSION["nom"] ?? null;
 $mdp = $_SESSION["mdp"] ?? null;
 $statut = $_SESSION['statut'] ?? null;
 
-function NoConnect(){
+function NoConnect()
+{
     echo "Tu n'es pas connecté<br>";
     echo "<a href='register.php'>Connexion</a><br>";
     echo "<a href='login.php'>Inscription</a><br>";
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>Project Y</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <div class="header">
-            <nav class="profile_img">
-                <a href="profile.php"><img src="images/person-circle-outline.svg" alt="profile" width="70" height="70"></a>
-            </nav>
-            <div class="logo">
-                <img src="images/logo.jpg" alt="logo" width="70" height="70">
-            </div>
-            <?php
-            if (isset($pseudo)){
-            ?>
-                <div class="log-out">
-                    <a href="login.php"><img src="images/log-out.svg" alt="log-out" width="70" height="70"></a>
-                </div>
-            <?php
-            }
-            ?>
+<?php
+include("includes/head.php");
+?>
+
+<body>
+    <div class="header">
+        <nav class="profile_img">
+            <a href="profile.php"><img src="images/person-circle-outline.svg" alt="profile" width="70" height="70"></a>
+        </nav>
+        <div class="logo">
+            <img src="images/logo.jpg" alt="logo" width="70" height="70">
         </div>
-
-        <br><br><br><br><br>
-
         <?php
-        if (isset($pseudo)){
+        if (isset($nom)) {
         ?>
-            <div class="contenu">
-                <div class="gauche">
-                </div>
-                <div class="profile">
-                    <nav class="retour">
-                        <a href="#"><img src="images/arrow-left.svg" alt="retour" width="40" height="40"></a>
-                    </nav>
-
-                    <div class="fond_profile">
-                        <img src="images/fond_profile.jpeg" alt="fond_profile">
-                    </div>
-                    <div class="profile_img">
-                        <img src="images/person-circle-outline.svg" alt="profile" width="70" height="70">
-                    </div>
-
-                    <div class="edit_button" align="right">
-                        <button type="submit">Edit profile</button>
-                    </div>
-
-                    <p class="name">name</p>
-                    <p> <b>0</b> Following  <b>0</b> Followers</p>
-
-
-                    <div class="last_info">
-                        <div>Posts</div>
-                        <div>Replies</div>
-                        <div>Highlights</div>
-                        <div>Media</div>
-                        <div>Likes</div>
-                    </div>
-                    
-                    <div class="content_box">
-                    </div>
-                    
-                </div>
-                <div class="droite">
-                </div>
+            <div class="log-out">
+                <a href="login.php"><img src="images/log-out.svg" alt="log-out" width="70" height="70"></a>
             </div>
         <?php
-        }else{
-            NoConnect();
         }
         ?>
-        
-        <div class="footer">
-            <nav class="navigateur">
-                <ol>
-                    <li class="lien"><a href="accueil.php"><img src="images/home.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="setting.php"><img src="images/settings.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="message.php"><img src="images/paper-plane.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                    <li class="lien"><a href="partage.php"><img src="images/share.svg" alt="profil" class="image" width="42" height="42"></a></li>
-                </ol>
-            </nav>
+    </div>
+
+    <br><br><br><br><br>
+
+    <?php
+    if (isset($nom)) {
+    ?>
+        <div class="contenu">
+            <div class="gauche">
+            </div>
+            <div class="profile">
+                <nav class="retour">
+                    <a href="#"><img src="images/arrow-left.svg" alt="retour" width="40" height="40"></a>
+                </nav>
+
+                <div class="fond_profile">
+                    <img src="images/fond_profile.jpeg" alt="fond_profile">
+                </div>
+                <div class="profile_img">
+                    <img src="images/person-circle-outline.svg" alt="profile" width="70" height="70">
+                </div>
+
+                <div class="edit_button" align="right">
+                    <button type="submit">Edit profile</button>
+                </div>
+
+                <p class="name">name</p>
+                <p> <b>0</b> Following <b>0</b> Followers</p>
+
+
+                <div class="last_info">
+                    <div>Posts</div>
+                    <div>Replies</div>
+                    <div>Highlights</div>
+                    <div>Media</div>
+                    <div>Likes</div>
+                </div>
+
+                <div class="content_box">
+                </div>
+
+            </div>
+            <div class="droite">
+            </div>
         </div>
-    </body>
-</html>
+    <?php
+    } else {
+        NoConnect();
+    }
+    ?>
+
+    <?php
+    include("includes/footer.php");
+    ?>
