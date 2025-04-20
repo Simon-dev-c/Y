27b6682c2e885a6eab375a039b2ee6596035cc3a
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-20 14:04:35 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 $nom = $_SESSION["nom"] ?? null;
-$mdp = $_SESSION["mdp"] ?? null;
 $statut = $_SESSION['statut'] ?? null;
 
 
@@ -9,7 +8,7 @@ include("includes/head2.php");
 ?>
 
 <body>
-    <form>
+    <form action="sql/connexion.php" method="POST">
         <div class="logo1">
             <img src="images/logo.jpg" alt="logo" width="70" height="70">
         </div>
@@ -17,21 +16,23 @@ include("includes/head2.php");
         if (!isset($nom)) {
         ?>
             <h2>Se connecter</h2>
-            <label class="input_box"><input name="identifiant" placeholder="Identiant"></label><br><br>
-            <label class="input_box"><input name="password" type="password" placeholder="password" maxlength="25"></label><br><br>
-            <label><input name="button" type="button" value="Se connecter"></label><br>
-            <label><a href="register.php">S'identifier</a></label><br>
-            <label><a href="accueil.php">Accueil</a></label>
-            <label class="mots_passe"><a href="">Mots de passe oublier</a></label><br>
+
+            <label><input type="text" name="nom" placeholder="nom" required="required"></label>
+            <br><br>
+            <label><input type="password" name="mdp" placeholder="Mot de passe" required="required" maxlength="25"></label>
+            <br><br>
+            <label><input name="button" type="submit" value="Se connecter"></label><br>
+            <label><a href="register.php">Pas de compte ?</a></label><br>
+
         <?php
         } else {
         ?>
             <h2>Vous êtes déjà connecté !</h2>
-            <a href='accueil.php'>Accueil</a><br>
 
         <?php
         }
         ?>
+        <a href='accueil.php'>Accueil</a><br>
     </form>
 </body>
 
