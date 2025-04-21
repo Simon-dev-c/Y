2f186d93c650d369428437b5c1042e7808c52afa
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-04-21 18:16:58 +0200

    head maj

--- setting.php
+++ setting.php
@@ -20,26 +20,6 @@ include("sql/recup_bio.php");
 include("includes/head.php");
 ?>
 
-<body>
-    <div class="header">
-        <nav class="profil">
-            <a href="profile.php"><img src="images/person-circle-outline.svg" alt="profil" width="70" height="70"></a>
-        </nav>
-        <div class="logo">
-            <img src="images/logo.jpg" alt="logo" width="70" height="70">
-        </div>
-        <?php
-        if (isset($nom)) {
-        ?>
-            <div class="log-out">
-                <a href="logout.php"><img src="images/log-out.svg" alt="log-out" width="70" height="70"></a>
-            </div>
-        <?php
-        }
-        ?>
-
-    </div>
-
     <br><br><br><br><br> <!-- Important pour ne pas mettre le contenu derrière le header -->
     <?php
     if (!isset($nom)) {
