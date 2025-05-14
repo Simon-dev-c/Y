81b2581d100f3a389b9b1bf679438d7f87568c3a
Simon-dev-c <arnaudsimonarnaud@gmail.com>
2025-05-14 10:17:32 +0200

    Recup post accueil

--- accueil.php
+++ accueil.php
@@ -1,49 +1,45 @@
 <?php
 session_start();
-$id = $_SESSION["id"] ?? null;
-$nom = $_SESSION["nom"] ?? null;
+$id = $_SESSION['id'] ?? null;
+$nom = $_SESSION['nom'] ?? null;
 $statut = $_SESSION['statut'] ?? null;
 
-function NoConnect(){
+function NoConnect()
+{
     echo "Tu n'es pas connecté<br>";
     echo "<a href='login.php'>Connexion</a><br>";
     echo "<a href='register.php'>Inscription</a><br>";
 }
-
 
 ?>
 <?php
 include("includes/head.php");
 ?>
 
-        <br><br><br><br><br> <!-- Important pour ne pas mettre le contenu derrière le header -->
+<div class="contenu">
+    <div class="post">
         <?php
-        if (!isset($nom)){
+        if (!isset($nom)) {
             NoConnect();
-        }else{
+        } else {
         ?>
-        <div class="contenu">
-            <div class="gauche">
+            <h2>Fil d'actualité</h2>
+            <form action="ajouter_post.php" method="POST" class="post-form">
+                <textarea name="post" rows="4" placeholder="Quoi de neuf ?"></textarea>
+                <button type="submit">Publier</button>
+            </form>
+            <div class="posts">
+                <h3>Publications récentes :</h3>
+                <?php
+                include("sql/recup_post.php");
+                ?>
             </div>
-            <div class="post">
-                <form action="ajouter_post.php" method="POST">
-                    <label>Message :<textarea name="post" rows="5"></textarea></label>
-                    <br>
-                    <label><input name="button" type="submit" value="Envoyer"></label><br>
-                </form>
-                <table class="posts_other">
-
-                </table>
-            </div>
-            <div class="droite">
-            </div>
-        </div>
         <?php
         }
         ?>
-        <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>fgdgfdgfdgd<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-        
+    </div>
+</div>
 
-        <?php
-        include("includes/footer.php");
+<?php
+include("includes/footer.php");
 ?>
